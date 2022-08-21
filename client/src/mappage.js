946cda3783f8f49f6a94b58216ae8eff20a9aa26
ashlyn <68078477+ashlyn12923190@users.noreply.github.com>
2022-08-21 02:15:42 -0400

    Update mappage.js

--- client/src/mappage.js
+++ client/src/mappage.js
@@ -1,1 +1,16 @@
-
+import MapHeader from "./components/mapheader";
+import BottomNavBar from "./components/bottomnavbar";
+import AlerterInfo from "./components/alerterinfo";
+import GoogleMap from "./components/googlemap";
+import AlertHeader from "./components/alertheader";
+function MapPage() {
+    return (
+      <div>
+        <MapHeader />
+        <AlertHeader />
+        <AlerterInfo />
+        <GoogleMap />
+        <BottomNavBar />
+      </div>
+    );
+  }
