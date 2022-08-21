2e34f2de772f2c0eff2b45159b22204bed33040c
sedhha <activity.schoolsh2@gmail.com>
2022-08-21 11:06:16 +0530

    Client Changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,42 +1,21 @@
 import './App.css';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React from 'react';
 import LoginPage from './loginpage';
 import SignupPage from './signuppage';
-import UserOverview from './useroverview'
-import EnrollPage from './enroll'
-
+import UserOverview from './useroverview';
+import EnrollPage from './enroll';
 
 function App() {
-  const [data, setData] = React.useState(null);
-
-  React.useEffect(() => {
-    fetch("/api")
-      .then((res) => res.json())
-      .then((data) => setData(data.message));
-  }, []);
-
   return (
     <Router>
-      <div className="App">
-        <Switch>
-          <Route path="/login">
-            <LoginPage />
-          </Route>
-
-          <Route path="/signup">
-            <SignupPage />
-          </Route>
-          <Route path="/user">
-          {/* <header className="App-header">
-              <p>{!data ? "Loading..." : data}</p>
-            </header> */}
-            <UserOverview />
-          </Route>
-          <Route path="/enroll">
-            <EnrollPage />
-          </Route>
-        </Switch>
+      <div className='App'>
+        <Routes>
+          <Route path='/login' element={<LoginPage />}></Route>
+          <Route path='/signup' element={<SignupPage />}></Route>
+          <Route path='/user' element={<UserOverview />}></Route>
+          <Route path='/enroll' element={<EnrollPage />}></Route>
+        </Routes>
       </div>
     </Router>
   );
--- client/src/components/signinform.js
+++ client/src/components/signinform.js
@@ -1,46 +1,61 @@
 import React from 'react';
+import { useNavigate } from 'react-router';
 import './login.css';
 const SigninForm = () => {
-    const [email,setEmail] = React.useState(""); 
-    const [password,setPassword] = React.useState(""); 
-    const rawResponse = () => {
-        console.log('Making a request');
-        fetch('http://localhost:3001/post/login', {
-          method: 'POST',
-          headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({email: email, password: password}),
-        })
-          .then((res) =>
-            res
-              .json()
-              .then((data) => {
-                console.log('Got the response = ', data);
-              })
-              .catch((error) =>
-                console.log('Error occured while parsing json = ', error.message)
-              )
+  const navigate = useNavigate();
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
+  const rawResponse = () => {
+    console.log('Making a request');
+    fetch('http://localhost:3001/post/login', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ email: email, password: password }),
+    })
+      .then((res) =>
+        res
+          .json()
+          .then((data) => {
+            console.log('Got the response = ', data);
+            if (data.isLoggedIn) {
+              navigate('/user');
+            }
+          })
+          .catch((error) =>
+            console.log('Error occured while parsing json = ', error.message)
           )
-          .catch((error) => {
-            console.log('Error while trying to make request: ', error.message);
-          });
-      };
+      )
+      .catch((error) => {
+        console.log('Error while trying to make request: ', error.message);
+      });
+  };
 
-    return ( 
-        <div>
-        <div className='input-login'>
-            <input type="email" placeholder="Email" required onChange = {(e) => setEmail(e.target.value)}></input>
-        </div>
-        <div className='input-login'>
-            <input type="password" placeholder="Password" required onChange = {(e) => setPassword(e.target.value)}></input>
-        </div>
-        <div className="SigninButton">
-            <button type="submit" value="Sign in" onClick = {rawResponse}>Sign In</button>
-        </div>
-        </div>
-     );
-}
- 
+  return (
+    <div>
+      <div className='input-login'>
+        <input
+          type='email'
+          placeholder='Email'
+          required
+          onChange={(e) => setEmail(e.target.value)}></input>
+      </div>
+      <div className='input-login'>
+        <input
+          type='password'
+          placeholder='Password'
+          required
+          onChange={(e) => setPassword(e.target.value)}></input>
+      </div>
+      <div className='SigninButton'>
+        <button type='submit' value='Sign in' onClick={rawResponse}>
+          Sign In
+        </button>
+      </div>
+    </div>
+  );
+};
+
 export default SigninForm;
