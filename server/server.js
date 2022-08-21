c7def0ee914f23255ed45ad06c81ba9f9789f1d2
Tony <78298444+evbesho@users.noreply.github.com>
2022-08-20 22:01:04 -0600

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const cors = require("cors");
+import express from 'express'
+import { MongoClient, ServerApiVersion } from 'mongodb'
+import cors from 'cors'
 const app = express();
 app.use(express.json());
 app.use(cors());
