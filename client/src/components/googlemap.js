c484f67a0b86386341a12e3721c15563b12acb1d
ashlyn <68078477+ashlyn12923190@users.noreply.github.com>
2022-08-21 05:25:52 -0400

    added map

--- client/src/components/googlemap.js
+++ client/src/components/googlemap.js
@@ -1,10 +1,30 @@
 import './usm.css';
-const GoogleMap = () => {
-    return ( 
-    <div id="mapholder">
+import { GoogleMap,withScriptjs,withGoogleMap,Marker } from 'react-google-maps';
+// import { Wrapper, Status } from "@googlemaps/react-wrapper";
+const MyMap = () => {
 
-    </div>
+return (
+<GoogleMap defaultZoom={15} defaultCenter={{lat:43.64709607777706,lng:-79.39544712511132}}>
+
+    <Marker position={{lat:43.64709607777706,lng:-79.39544712511132}}>
+    </Marker>
+</GoogleMap>
+
      );
 }
- 
-export default GoogleMap;
+
+const WrappedMap=withScriptjs(withGoogleMap(MyMap))
+
+export default function GoogleMapC(){
+    return(
+        <div className="okaymap" style={{width: '100%', height: '320px'}}>
+            <WrappedMap googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyAHOGKMyl_Ta_rVt5DtVSuhk_5qI7SUWPk`}
+            loadingElement={<div style={{height:"320px"}}/>}
+            containerElement={<div style={{height:"320px"}}/>}
+            mapElement={<div style={{height:"100%"}}/>}
+           /> 
+        </div>
+
+    )
+}
+
