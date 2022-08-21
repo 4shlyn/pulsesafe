16ace66bdad75b819e253178f40748ebd224baf6
Tony <78298444+evbesho@users.noreply.github.com>
2022-08-20 21:26:20 -0600

    Update signupform.js

--- client/src/components/signupform.js
+++ client/src/components/signupform.js
@@ -1,15 +1,44 @@
 import './signup.css';
+import React from 'react';
 const SignupForm = () => {
+    const [name,setName] = React.useState("");
+    const [email,setEmail] = React.useState(""); 
+    const [password,setPassword] = React.useState(""); 
+    const rawResponse = () => {
+        console.log('Making a request');
+        fetch('http://localhost:3001/post/signup', {
+          method: 'POST',
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ name: name, email: email, password: password}),
+        })
+          .then((res) =>
+            res
+              .json()
+              .then((data) => {
+                console.log('Got the response = ', data);
+              })
+              .catch((error) =>
+                console.log('Error occured while parsing json = ', error.message)
+              )
+          )
+          .catch((error) => {
+            console.log('Error while trying to make request: ', error.message);
+          });
+      };
+
     return ( 
-        <form>
+        <div>
         <div className='input-signup'>
-            <input type="text" placeholder="Full Name" id="name" required></input>
+            <input type="text" placeholder="Full Name" id="name" required onChange = {(e) => setName(e.target.value)}></input>
         </div>
         <div className='input-signup'>
-            <input type="email" placeholder="Email" id="mail" required></input>
+            <input type="email" placeholder="Email" id="mail" required onChange = {(e) => setEmail(e.target.value)}></input>
         </div>
         <div className='input-signup'>
-            <input type="password" placeholder="Password" id="pass" required></input>
+            <input type="password" placeholder="Password" id="pass" required onChange = {(e) => setPassword(e.target.value)}></input>
         </div>
         <div className='pass-confirm'>
             <input type="password" placeholder="Confirm Password" id="pass2" required></input>
@@ -19,10 +48,13 @@ const SignupForm = () => {
             <div>I agree to the <a href="">Terms and Conditions</a></div>
         </div>
         <div className="SignUpButton">
-            <input type="submit" value="Sign up" id="sign"></input>
+            <button id="sign" onClick = {rawResponse}>Sign Up</button>
         </div>
-        </form>
+        </div>
      );
 }
 
+
+
+ 
 export default SignupForm;
